3ccd4aed7a9ed4745d7565f82c2ab9ff602f56f2
MB-HP <mohamadbarmo@gmail.com>
2021-04-20 22:24:56 +0300

    end of 27 sessions django jobboard

--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from .models import Info
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
 
 def send_message(request):
-    pass
+    myinfo=Info.objects.last()
+    if request.method=="POST":
+        subject=request.POST['subject']
+        mail=request.POST['email']
+        message=request.POST['message']
+        name=request.POST['name']
+        send_mail(
+        subject,
+        message,
+        settings.EMAIL_HOST_USER,
+        [mail],
+        
+)
+    
+    return render(request,'contact/contact.html',{'myinfo':myinfo})
 
 
 
--- job/urls.py
+++ job/urls.py
@@ -2,12 +2,25 @@ from django.contrib import admin
 from django.urls import path , include
 
 from . import views
+from . import api
+
+
+
 app_name='job'
 
 urlpatterns = [
     path('',views.job_list,name='job_list' ),
     path('add',views.add_job, name='add_job' ),
-   # path('<int:id>',views.job_details, name='job_detail' ),
-   path('<str:slug>',views.job_details, name='job_detail' ),
+    # path('<int:id>',views.job_details, name='job_detail' ),
+    path('<str:slug>',views.job_details, name='job_detail' ),
+    path('api/jobs',api.job_list_api, name='job_list_api' ),
+    path('api/jobs/<int:id>',api.job_detail_api, name='job_detail_api' ),
    
+
+
+   ## class based views 
+    path('api/v2/jobs',api.JobListApi.as_view(), name='job_list_api' ),
+    path('api/v2/jobscreate',api.JobListApiCreate.as_view(), name='job_list_api' ),
+    path('api/v2/jobs/<int:id>',api.JobDeatail.as_view(), name='job_detail_api' ),
+
 ]
